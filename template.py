2cb0d0c84b3fdd4979f0b64f2de5f00f92f65fe0
Wolfgang Münder <wolfgang.muender@gmail.com>
2022-12-18 06:07:42 +0100

    Update template

--- template.py
+++ template.py
@@ -2,21 +2,25 @@ import os
 import time
 
 
-def main(puzzle_input):
+def solve(puzzle_input):
     print("Solution 1: {}".format(puzzle_input))
     print("Solution 2: {}".format(puzzle_input))
 
 
-if __name__ == "__main__":
+def main():
     input_filename = os.path.basename(__file__).replace("aoc", "input").replace("py", "txt")
     with open("input/{}".format(input_filename)) as f:
         content = f.read().splitlines()
 
     start = time.time()
-    main(content)
+    solve(content)
     end = time.time()
     diff = (end - start)
     if diff >= 1:
         print("The solutions took {}s".format(round(diff)))
     else:
         print("The solutions took {}ms".format(round(diff * 1000)))
+
+
+if __name__ == "__main__":
+    main()
