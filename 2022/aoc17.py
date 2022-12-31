5f898b128d327083aa3de505efdd70cd380c7c59
Wolfgang Münder <wolfgang.muender@gmail.com>
2022-12-31 13:42:53 +0100

    Add solution for day 17
    * part 2

--- 2022/aoc17.py
+++ 2022/aoc17.py
@@ -1,5 +1,3 @@
-import collections
-import json
 import os
 import time
 
@@ -9,38 +7,72 @@ EMPTY = "|.......|"
 def solve(puzzle_input):
     jet_pattern = list(puzzle_input[0])
 
-    cave = _fill_cave(jet_pattern, 2022)
+    cave_heights = _fill_cave(jet_pattern, 2022)
 
-    print("Solution 1: {}".format(len(cave)))
+    print("Solution 1: {}".format(sum(cave_heights)))
 
-    print("Solution 2: {}".format(puzzle_input))
+    cycle_length = 5 * len(jet_pattern)
+    cycle_start = None
+
+    cave_heights = _fill_cave(jet_pattern, 5 * cycle_length)
+
+    while not cycle_start:
+        if cycle_length > len(cave_heights):
+            break
+
+        for i in range(0, len(cave_heights)):
+            if i + 2 * cycle_length == len(cave_heights):
+                break
+            if cave_heights[i] == cave_heights[i + cycle_length]:
+                window_1 = cave_heights[i:i + cycle_length]
+                window_2 = cave_heights[i + cycle_length:i + 2 * cycle_length]
+                if window_1 == window_2:
+                    cycle_start = i
+                    break
+
+        if not cycle_start:
+            cycle_length += 5
+
+    num_rocks = 1000000000000
+    num_cycles = (num_rocks - cycle_start) // cycle_length
+    remaining = (num_rocks - cycle_start) % cycle_length
+
+    cycle_height = sum(cave_heights[cycle_start:cycle_start + cycle_length])
+
+    print("Solution 2: {}".format(sum(cave_heights[0:cycle_start + remaining]) + num_cycles * cycle_height))
 
 
 def _fill_cave(jet_pattern, max_num_rocks):
-    cave = [list("+-------+")]
+    cave = []
+    cave_heights = []
 
     num_rocks = 0
     curr_jet_index = 0
     while num_rocks < max_num_rocks:
+        old_height = len(cave)
+
         num_empty = cave.count(list(EMPTY))
-        for i in range(0, 3-num_empty):
+        for i in range(0, 3 - num_empty):
             cave.append(list(EMPTY))
 
+        rock_pos = len(cave)
         _append_rock(cave, num_rocks)
 
         go_on = True
         while go_on:
             curr_jet = jet_pattern[curr_jet_index]
             curr_jet_index = (curr_jet_index + 1) % len(jet_pattern)
-            _shift(cave, curr_jet)
-            go_on = _move_down(cave)
-        _settle(cave)
+            _shift(cave, curr_jet, rock_pos)
+            go_on = _move_down(cave, rock_pos)
+            rock_pos -= 1
+
+        cave_heights.append(len(cave) - old_height)
+
+        _settle(cave, rock_pos)
 
         num_rocks += 1
 
-    cave.pop(0)
-
-    return cave
+    return cave_heights
 
 
 def _append_rock(cave, num_rocks):
@@ -66,46 +98,49 @@ def _append_rock(cave, num_rocks):
         raise Exception("Whoot?")
 
 
-def _shift(cave, jet):
-    for i in range(0, len(cave)):
+def _shift(cave, jet, rock_pos):
+    for i in range(rock_pos, len(cave)):
         if jet == "<":
             for j in range(1, 8):
                 if cave[i][j] == "@":
-                    if cave[i][j-1] != ".":
+                    if cave[i][j - 1] != ".":
                         return
                     else:
                         break
         elif jet == ">":
             for j in range(7, 0, -1):
                 if cave[i][j] == "@":
-                    if cave[i][j+1] != ".":
+                    if cave[i][j + 1] != ".":
                         return
                     else:
                         break
 
-    for i in range(0, len(cave)):
+    for i in range(rock_pos, len(cave)):
         if jet == "<":
             for j in range(1, 8):
                 if cave[i][j] == "@":
-                    cave[i][j-1] = "@"
+                    cave[i][j - 1] = "@"
                     cave[i][j] = "."
         elif jet == ">":
             for j in range(7, 0, -1):
                 if cave[i][j] == "@":
-                    cave[i][j+1] = "@"
+                    cave[i][j + 1] = "@"
                     cave[i][j] = "."
 
 
-def _move_down(cave):
-    for i in range(0, len(cave)):
+def _move_down(cave, rock_pos):
+    if len(cave) == 1:
+        return False
+
+    for i in range(rock_pos, len(cave)):
         for j in range(1, 8):
-            if cave[i][j] == "@" and cave[i-1][j] not in (".", "@"):
+            if cave[i][j] == "@" and cave[i - 1][j] not in (".", "@"):
                 return False
 
-    for i in range(0, len(cave)):
+    for i in range(rock_pos, len(cave)):
         for j in range(1, 8):
             if cave[i][j] == "@":
-                cave[i-1][j] = cave[i][j]
+                cave[i - 1][j] = cave[i][j]
                 cave[i][j] = "."
 
     if cave[-1] == list(EMPTY):
@@ -114,16 +149,19 @@ def _move_down(cave):
     return True
 
 
-def _settle(cave):
-    for i in range(0, len(cave)):
+def _settle(cave, rock_pos):
+    for i in range(rock_pos, len(cave)):
         for j in range(1, 8):
             if cave[i][j] == "@":
                 cave[i][j] = "#"
 
+    while len(cave) > 100:
+        cave.pop(0)
+
 
 def _print(cave):
     for i in range(0, len(cave)):
-        print("".join(cave[-(1+i)]))
+        print("".join(cave[-(1 + i)]))
 
 
 def main():
