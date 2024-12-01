a0634b90c3b96b213e67823d40a9fc68011725e7
Wolfgang Münder <wolfgang.muender@gmail.com>
2024-12-01 14:43:39 +0100

    Add solution for day 1/2024

---
+++ 2024/aoc2024_01.py
@@ -0,0 +1,70 @@
+import os
+import time
+
+TEST_SOLUTION1 = 11
+TEST_SOLUTION2 = 31
+
+
+def solve(puzzle_input):
+    list1 = []
+    list2 = []
+    for line in puzzle_input:
+        location_id1, location_id2 = line.split("   ")
+        list1.append(int(location_id1))
+        list2.append(int(location_id2))
+
+    total_distance = sum([abs(a - b) for a, b in zip(sorted(list1), sorted(list2))])
+    similarity_score = sum([a * sum([1 if a==b else 0 for b in list2]) for a in list1])
+
+    return total_distance, similarity_score
+
+
+def main():
+    test_input_file = "input/" + os.path.basename(__file__).replace("aoc", "testinput").replace("py", "txt")
+    test_input_file2 = test_input_file.replace(".txt", "-2.txt")
+    if os.path.isfile(test_input_file):
+        start = time.time()
+        with open(test_input_file) as f:
+            content1 = f.read().splitlines()
+        if os.path.isfile(test_input_file2):
+            with open(test_input_file2) as f:
+                content2 = f.read().splitlines()
+            solution1, _ = solve(content1)
+            _, solution2 = solve(content2)
+        else:
+            solution1, solution2 = solve(content1)
+        if solution1 != TEST_SOLUTION1:
+            print(f"TEST solution 1 '{solution1}' not correct!")
+            return
+        if solution2 != TEST_SOLUTION2:
+            print(f"TEST solution 2 '{solution2}' not correct!")
+            return
+        end = time.time()
+        print_diff(end - start, True)
+    else:
+        open(test_input_file, 'a').close()
+
+    input_file = "input/" + os.path.basename(__file__).replace("aoc", "input").replace("py", "txt")
+    if os.path.isfile(input_file):
+        with open(input_file) as f:
+            content = f.read().splitlines()
+        start = time.time()
+        solution1, solution2 = solve(content)
+        print("Solution 1: {}".format(solution1))
+        print("Solution 2: {}".format(solution2))
+        end = time.time()
+        print_diff(end - start, False)
+    else:
+        open(input_file, 'a').close()
+
+
+def print_diff(diff, is_test):
+    prefix = "TEST " if is_test else ""
+    if diff >= 1:
+        print("The {}solutions took {}s".format(prefix, round(diff)))
+    else:
+        print("The {}solutions took {}ms".format(prefix, round(diff * 1000)))
+
+
+if __name__ == "__main__":
+    main()
