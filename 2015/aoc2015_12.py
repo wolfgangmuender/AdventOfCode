9622d7061ff02bba310124c455cd5e1567922f5e
Wolfgang Münder <wolfgang.muender@gmail.com>
2025-01-12 23:27:49 +0100

    Add solution for day 12/2015

---
+++ 2015/aoc2015_12.py
@@ -0,0 +1,84 @@
+import json
+import os
+import time
+
+TEST_SOLUTION1 = 6
+TEST_SOLUTION2 = 4
+
+
+def solve(puzzle_input):
+    json_string = puzzle_input[0]
+
+    the_json = json.loads(json_string)
+
+    return count(the_json, False), count(the_json, True)
+
+
+def count(the_json, ignore_red):
+    the_count = 0
+
+    if isinstance(the_json, dict):
+        if ignore_red and "red" in the_json.values():
+            return 0
+        else:
+            for v in the_json.values():
+                the_count += count(v, ignore_red)
+        pass
+    elif isinstance(the_json, list):
+        for v in the_json:
+            the_count += count(v, ignore_red)
+    elif isinstance(the_json, int):
+        the_count = the_json
+
+    return the_count
+
+
+def main():
+    test_input_file = "input/" + os.path.basename(__file__).replace("aoc", "testinput").replace("py", "txt")
+    test_input_file2 = test_input_file.replace(".txt", "-2.txt")
+    if os.path.isfile(test_input_file):
+        start = time.time()
+        with open(test_input_file) as f:
+            content1 = f.read().splitlines()
+        if os.path.isfile(test_input_file2):
+            with open(test_input_file2) as f:
+                content2 = f.read().splitlines()
+            solution1, _ = solve(content1)
+            _, solution2 = solve(content2)
+        else:
+            solution1, solution2 = solve(content1)
+        if solution1 != TEST_SOLUTION1:
+            print(f"TEST solution 1 '{solution1}' not correct!")
+            return
+        if solution2 != TEST_SOLUTION2:
+            print(f"TEST solution 2 '{solution2}' not correct!")
+            return
+        end = time.time()
+        print_diff(end - start, True)
+    else:
+        open(test_input_file, 'a').close()
+
+    input_file = "input/" + os.path.basename(__file__).replace("aoc", "input").replace("py", "txt")
+    if os.path.isfile(input_file):
+        with open(input_file) as f:
+            content = f.read().splitlines()
+        start = time.time()
+        solution1, solution2 = solve(content)
+        print("Solution 1: {}".format(solution1))
+        print("Solution 2: {}".format(solution2))
+        end = time.time()
+        print_diff(end - start, False)
+    else:
+        open(input_file, 'a').close()
+
+
+def print_diff(diff, is_test):
+    prefix = "TEST " if is_test else ""
+    if diff >= 1:
+        print("The {}solutions took {}s".format(prefix, round(diff)))
+    else:
+        print("The {}solutions took {}ms".format(prefix, round(diff * 1000)))
+
+
+if __name__ == "__main__":
+    main()
